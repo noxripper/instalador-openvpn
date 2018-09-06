81b0109f0bc1cccfe8048e1a48cf1621cc195a77
Marcos Aurélio Glovatinski <noxripper@gmail.com>
2018-09-06 09:52:48 -0300

    Update Instalador-openvpn.sh

--- Instalador-openvpn.sh
+++ Instalador-openvpn.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #
-# https://github.com/Nyr/openvpn-install
+# https://github.com/noxripper/instalador-openvpn
 #
-# Copyright (c) 2013 Nyr. Released under the MIT License.
+# Copyright (c) 2013 Nox Ripper. Released under the MIT License.
 
 
 # Detect Debian users running the script with "sh" instead of bash
